4f3ef781bd69f85f2a8add05dc5095a1db43e10c
Jameershake <jameershaik667@gmail.com>
2025-10-08 22:50:00 +0530

    Initial commit - EarnSure backend setup

--- server/server.js
+++ server/server.js
@@ -32,12 +32,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // CORS - Update this section
+// CORS
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://127.0.0.1:5173'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:5175',
+    'http://127.0.0.1:5173',
+    'https://earnsure.vercel.app'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 
 
 // Routes
